132bedb246acbb685eed1d05c6b9ac92c169cb63
giseung.lee <goodexample.lee@gmail.com>
2026-02-16 16:21:48 +0900

    Initial commit: Hello World 🎮

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🎮 미니 게임 사이트</h1>
+        <p>Hello World!</p>
+        <p style={{ fontSize: '1rem', opacity: 0.7 }}>
+          Coming Soon: 테트리스 | 2048 | Fruit Ninja
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
